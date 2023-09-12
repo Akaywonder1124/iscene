1c7b470931ae1c60021623745be39a8a9ba278c1
Akaywonder1124 <akaycopy@gmail.com>
2023-09-12 19:54:03 +0100

    commit

--- js/script.js
+++ js/script.js
@@ -1,18 +1,90 @@
-//APIKey = e2121f4c0901b3856a1985143270d754
-//AccessToken = eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMjEyMWY0YzA5MDFiMzg1NmExOTg1MTQzMjcwZDc1NCIsInN1YiI6IjYzZGMwNDU4YTkxMTdmMDA5M2NmMjliYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._n71JBLy1x5_ydllc7My7H9upSc7TeWswlDiGtknZDY
-
 const global = {
   currentPage: window.location.pathname,
 };
 
+async function displayPopularMovies() {
+  const { results } = await fetchAPIData("movie/popular");
+  results.forEach((movie) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = ` <div class="card">
+          <a href="movie-details.html?id=${movie.id}">
+            ${
+              movie.poster_path
+                ? `<img
+              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              class="card-img-top"
+              alt=${movie.title}
+            />`
+                : `<img
+              src="images/no-image.png"
+              class="card-img-top"
+              alt="Movie Title"
+            />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${movie.release_date}</small>
+            </p>
+          </div>
+        </div>`;
+    document.querySelector("#popular-movies").appendChild(div);
+  });
+}
+async function displayTVShows() {
+  const { results } = await fetchAPIData("movie/popular");
+  results.forEach((movie) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = ` <div class="card">
+          <a href="movie-details.html?id=${movie.id}">
+            ${
+              movie.poster_path
+                ? `<img
+              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              class="card-img-top"
+              alt=${movie.title}
+            />`
+                : `<img
+              src="images/no-image.png"
+              class="card-img-top"
+              alt="Movie Title"
+            />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${movie.release_date}</small>
+            </p>
+          </div>
+        </div>`;
+    document.querySelector("#popular-movies").appendChild(div);
+  });
+}
 //Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
-  //LOL, don't COPY
-  //You can get yours at https://www.themoviedb.org/
-
+  //LOL, don't COPY. You can get yours at https://www.themoviedb.org/
   const API_KEY = "e2121f4c0901b3856a1985143270d754";
   const API_URL = "https://api.themoviedb.org/3/";
+  showSpinner();
+  const response = await fetch(
+    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US `
+  );
+  const data = await response.json();
+  hideSpinner();
+  return data;
 }
+
+function showSpinner() {
+  document.querySelector(".spinner").classList.add("show");
+}
+function hideSpinner() {
+  document.querySelector(".spinner").classList.remove("show");
+}
+
 function highlightActiveLink() {
   const links = document.querySelectorAll(".nav-link");
   links.forEach((link) => {
@@ -21,10 +93,12 @@ function highlightActiveLink() {
     }
   });
 }
+
 function init() {
   switch (global.currentPage) {
     case "/":
     case "/index.html":
+      displayPopularMovies();
       console.log("Home");
       break;
     case "/shows.html":
