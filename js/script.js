f254d5dcd79ec22b15fa91091417d011168cb5da
Akaywonder1124 <akaycopy@gmail.com>
2023-09-12 20:36:23 +0100

    commit

--- js/script.js
+++ js/script.js
@@ -33,19 +33,20 @@ async function displayPopularMovies() {
     document.querySelector("#popular-movies").appendChild(div);
   });
 }
+
 async function displayTVShows() {
-  const { results } = await fetchAPIData("movie/popular");
-  results.forEach((movie) => {
+  const { results } = await fetchAPIData("tv/popular");
+  results.forEach((show) => {
     const div = document.createElement("div");
     div.classList.add("card");
     div.innerHTML = ` <div class="card">
-          <a href="movie-details.html?id=${movie.id}">
+          <a href="shows.html?id=${show.id}">
             ${
-              movie.poster_path
+              show.poster_path
                 ? `<img
-              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              src="https://image.tmdb.org/t/p/w500${show.poster_path}"
               class="card-img-top"
-              alt=${movie.title}
+              alt=${show.name}
             />`
                 : `<img
               src="images/no-image.png"
@@ -55,15 +56,21 @@ async function displayTVShows() {
             }
           </a>
           <div class="card-body">
-            <h5 class="card-title">${movie.title}</h5>
+            <h5 class="card-title">${show.name}</h5>
             <p class="card-text">
-              <small class="text-muted">Release: ${movie.release_date}</small>
+              <small class="text-muted">Release: ${show.first_air_date}</small>
             </p>
           </div>
         </div>`;
-    document.querySelector("#popular-movies").appendChild(div);
+    document.querySelector("#popular-shows").appendChild(div);
   });
 }
+
+async function getMovieDetails() {
+  const movieId = window.location.search.split("=")[1];
+  const result = await fetchAPIData(`movies/${movieId}`);
+}
+
 //Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
   //LOL, don't COPY. You can get yours at https://www.themoviedb.org/
@@ -94,18 +101,23 @@ function highlightActiveLink() {
   });
 }
 
+function changeLogo() {
+  const logo = document.querySelector(".logo");
+  const footerLogo = document.querySelector(".logo span");
+  logo.textContent = "iscene";
+  footerLogo.textContent = "iscene";
+}
 function init() {
   switch (global.currentPage) {
     case "/":
     case "/index.html":
       displayPopularMovies();
-      console.log("Home");
       break;
     case "/shows.html":
-      console.log("Shows");
+      displayTVShows();
       break;
     case "/movie-details.html":
-      console.log("Movie-details.html");
+      getMovieDetails();
       break;
     case "/tv-details.html":
       console.log("Tv Shows");
@@ -115,6 +127,7 @@ function init() {
       break;
   }
   highlightActiveLink();
+  changeLogo();
 }
 
 document.addEventListener("DOMContentLoaded", init);
